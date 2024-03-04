130284576f736bdf890e155a8688c9f9c0469f80
Rin-t <rin.t.4921@gmail.com>
2024-03-04 23:52:08 +0900

    Initial Commit

---
+++ PracticeTCA/PracticeTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeTCAApp.swift
+//  PracticeTCA
+//
+//  Created by Rin on 2024/03/04.
+//
+
+import SwiftUI
+
+@main
+struct PracticeTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
